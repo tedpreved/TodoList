02427c832991f851b4eadac703fe0d16171ce15f
teodor <tedor.mytsyk@agilie.com>
2017-02-27 11:04:18 +0200

    add Anko

--- app/src/main/java/com/mytsyk/teo/todolist/data/realm/ui/home/HomeView.kt
+++ app/src/main/java/com/mytsyk/teo/todolist/data/realm/ui/home/HomeView.kt
@@ -1,7 +1,24 @@
 package com.mytsyk.teo.todolist.data.realm.ui.home
 
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mytsyk.teo.todolist.R
+
 /**
  * Created by teodor on 2/27/17.
  */
-class HomeView {
+class HomeView : Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_home, container)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+
 }
